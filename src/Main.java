4b06396a01f1d0ed99208d6502f7989bf6cf77eb
zedoCN <zedoCN@outlook.com>
2023-10-06 23:17:24 +0800

    结束

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
         //langProperties.store(Files.newOutputStream(p));
         //readAllLanguages();
         useAllLanguages(pluginPath);
+        useAllLanguages(vvmPath);
         //readAllLanguages(pluginPath);
         //findRemainingLang();
     }
